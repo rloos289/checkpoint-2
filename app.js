a893664f76be14eeb5b091f2e7ea118ed69cb585
grantibrees <grantibrees@gmail.com>
2020-06-28 16:32:42 -0600

    function names

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 
+let hero = {
+  isaac: {
+    charImage: "resources/characters/hero/Isaac_lBlade_Front.gif",
+    icon: "resources/characters/hero/hero-icons/Isaac.gif",
+    name: "Isaac"
+  },
+  mia: {
+    charImage: "resources/characters/hero/Mia_Mace_Front.gif",
+    icon: "resources/characters/hero/hero-icons/Mia.gif",
+    name: "Mia"
+  }
+}
 
+let resources = {
+  slimes: {
+    count: 0,
+    icon: "resources/resource-icons/Potion.gif"
+  },
+  gold: {
+    count: 0,
+    icon: "resources/resource-icons/Potion.gif"
+  },
+  health: {
+    count: 0,
+    icon: "resources/resource-icons/Water_of_Life.gif"
+  },
+  monsterHeads: {
+    count: 0,
+    icon: "resources/resource-icons/Flame_Shield.gif"
+  }
+}
 
 let weapons = { 
+  base: {
+    rustySword: {
+      name: "Rusty Sword",
+      description: "You found it on the ground",
+      level: 0,
+      unique: true,
+      icon: "resources/items/weapons/Rusty_Sword.gif",
+      slimeCost: 0
+    }
+  },
   levelOne: {
     broadAxe: {
       name: "Broad Axe",
@@ -249,80 +289,318 @@ let armor = {
   }
   }
 
-  let boots = { 
+let boots = { 
     levelOne: {
-      furCoat: {
-        name: "Fur Coat",
-        description: "Too hot in the summer, just right in the winter. Blocks Beasts a little",
+      furBoots: {
+        name: "Fur Boots",
+        description: "Too hot in the summer, too hot in the winter too. Kill slimes a tad quicker",
         level: 1,
-        blockBeast: 1,
+        slimeKillAdd: 1,
         unique: false,
-        icon: "resources/items/armor/Fur_Coat.gif",
+        icon: "resources/items/boots/Fur_Boots.gif",
         slimeCost: 5
       },
-      adeptsClothes: {
-        name: "Adept's Clothes",
-        description: "Breezy and light. Blocks Undead a little",
+      leatherBoots: {
+        name: "Leather Boots",
+        description: "Why aren't there more of these around? Kill slimes a little quicker",
         level: 1,
-        blockUndead: 1,
+        slimeKillAdd: 2,
         unique: true,
-        icon: "resources/items/armor/Adepts_Clothes.gif",
+        icon: "resources/items/boots/Leather_Boots.gif",
         slimeCost: 5
-      },
+      }
     },
     levelTwo: {
-      chainMail: {
-        name: "Chain Mail",
-        description: "Heavy, but moveable. Blocks Beasts somewhat",
+      knightsGreave: {
+        name: "Knight's Greave",
+        description: "Protect your shins. Kill slimes a bit quicker",
         level: 2,
-        blockBeast: 2,
+        slimeKillAdd: 3,
         unique: false,
-        icon: "resources/items/armor/Chain_Mail.gif",
+        icon: "resources/items/boots/Knights_Greave.gif",
         slimeCost: 10
       },
-      elvenShirt: {
-        name: "Elven Shirt",
-        description: "Silky and snappy. Blocks Undead somewhat",
+      leatherBoots: {
+        name: "Leather Boots",
+        description: "Werewolves stay away. Kill slimes quicker",
         level: 2,
-        blockUndead: 2,
+        slimeKillAdd: 4,
         unique: true,
-        icon: "resources/items/armor/Elven_Shirt.gif",
+        icon: "resources/items/boots/Silver_Greave.gif",
         slimeCost: 10
-      },
+      }
     },
     levelThree: {
-      plateMail: {
-        name: "Plate Mail",
-        description: "Heavy and protective. Blocks Beasts significantly",
+      goldenBoots: {
+        name: "Golden Boots",
+        description: "Kick butts with a shine. Kill slimes a decent amount quicker",
         level: 3,
-        blockBeast: 3,
+        slimeKillAdd: 5,
         unique: false,
-        icon: "resources/items/armor/Plate_Mail.gif",
+        icon: "resources/items/boots/Knights_Greave.gif",
         slimeCost: 15
       },
-      ninjaGarb: {
-        name: "Ninja Garb",
-        description: "Black and silent. Blocks Undead significantly",
+      ninjaSandals: {
+        name: "Ninja Sandals",
+        description: "Run up walls. Kill slimes a lot quicker",
         level: 3,
-        blockUndead: 3,
+        slimeKillAdd: 6,
         unique: true,
-        icon: "resources/items/armor/Ninja_Garb.gif",
+        icon: "resources/items/boots/Ninja_Sandals.gif",
         slimeCost: 15
-      },
+      }
     },
     levelFour: {
-      dragonScales: {
-        name: "Dragon Scales",
-        description: "Fully magical protection. Blocks Beasts and Undead completely",
+      ninjaSandals: {
+        name: "Ninja Sandals",
+        description: "Look glorious. Kill swaths of slimes",
         level: 4,
-        blockUndead: 100,
-        blockBeasts: 100,
+        slimeKillAdd: 7,
         unique: true,
-        icon: "resources/items/armor/Dragon_Scale.gif",
+        icon: "resources/items/boots/Dragon_Boots.gif",
         slimeCost: 30,
         goldCost: 30
       }
     }
+  }
+
+let monsters = {
+  slimes: {
+    weak: {
+      name: "Weak Slime",
+      image: "resources/monsters/slimes/Slime.gif",
+      level: 1
+    },
+    meaty: {
+      name: "Meaty Slime",
+      image: "resources/monsters/slimes/Slime_Beast.gif",
+      level: 2
+    },
+    chonky: {
+      name: "Chonky Slime",
+      image: "resources/monsters/slimes/Ooze.gif",
+      level: 3
     }
+  },
+  brigand: {
+    name "Brigand",
+    image: "resources/monsters/Brigand.gif"
+  },
+  beasts: {
+    goblin: {
+      name: "Goblin",
+      image: "resources/monsters/beasts/Alec_Goblin.gif",
+      level: 1
+    },
+    antLion: {
+      name: "Ant Lion",
+      image: "resources/monsters/beasts/Ant_Lion.gif",
+      level: 2
+    },
+    golem: {
+      name: "Earth Golem",
+      image: "resources/monsters/beasts/Golem.gif",
+      level: 3
+    },
+    woodWalker: {
+      name: "Wood Walker",
+      image: "resources/monsters/beasts/Wood_Walker.gif",
+      level: 4
+    },
+  },
+  undead: {
+    zombie: {
+      name: "Zombie",
+      image: "resources/monsters/undead/Zombie.gif",
+      level: 1
+    },
+    skeleton: {
+      name: "Skeleton",
+      image: "resources/monsters/undead/Skeleton.gif",
+      level: 2
+    },
+    ghoul: {
+      name: "Ghoul",
+      image: "resources/monsters/undead/Amaze.gif",
+      level: 3
+    },
+    slayer: {
+      name: "Slayer",
+      image: "resources/monsters/undead/Slayer.gif",
+      level: 4
+    },
+  }
+}
+
+let characters = {
+  unlocks: {
+    guildMaster: {
+      name: "Guild Master",
+      icon: "resources/characters/unlocks/Innkeeper.gif",
+      slimeCost: 20,
+    },
+    bladeSmith: {
+      name: "Blade Smith",
+      icon: "resources/characters/unlocks/Master_Hama.gif",
+      goldCost: 20,
+    }
+  },
+  hirelings: {
+    levelOne: {
+      spectralArmor: {
+        name: "Spectral Armor",
+        quote: `"Rattling noises"`,
+        level: 1,
+        health: 20,
+        unique: false,
+        icon: "resources/characters/hirelings/hireicons/Shadow_Babi.gif",
+        image: "resources/characters/hirelings/Azart.gif",
+        slimeCost: 5
+      },
+      jenna: {
+        name: "Jenna",
+        quote: "Are you saying that I'm insensitive!?",
+        level: 1,
+        health: 50,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Jenna.gif",
+        image: "resources/characters/hirelings/Azart.gif",
+        slimeCost: 10,
+        slots: {}
+      },
+      garet: {
+        name: "Garet",
+        quote: "Evil never prevails!",
+        level: 1,
+        health: 50,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Garet2.gif",
+        image: "resources/characters/hirelings/Garet_Mace_Front.gif",
+        slimeCost: 10,
+        slots: {}
+      }
+    },
+    levelTwo: {
+      sheba: {
+        name: "Sheba",
+        quote: "I wonder... Could it be the wind?",
+        level: 2,
+        health: 75,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Sheba.gif",
+        image: "resources/characters/hirelings/Sheba_Mace_Front.gif",
+        slimeCost: 15,
+        slots: {}
+      },
+      piers: {
+        name: "Piers",
+        quote: "Honor, plebeians, do you know it?",
+        level: 2,
+        health: 75,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Piers.gif",
+        image: "resources/characters/hirelings/Piers_lSword_Front.gif",
+        slimeCost: 15,
+        slots: {}
+      },
+      ivan: {
+        name: "Ivan",
+        quote: "Do not give up so easily...",
+        level: 2,
+        health: 75,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Ivan.gif",
+        image: "resources/characters/hirelings/Ivan_Staff_Front.gif",
+        slimeCost: 20,
+        slots: {}
+      }
+    },
+    levelThree: {
+      mendardi: {
+        name: "Menardi",
+        quote: "Let's make haste!",
+        level: 3,
+        health: 100,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Menardi.gif",
+        image: "resources/characters/hirelings/Menardi_Front.gif",
+        slimeCost: 30,
+        slots: {}
+      },
+      saturos: {
+        name: "Saturos",
+        quote: "Tread lightly...",
+        level: 3,
+        health: 105,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Saturos.gif",
+        image: "resources/characters/hirelings/Saturos_Front.gif",
+        slimeCost: 30,
+        slots: {}
+      },
+      felix: {
+        name: "Felix",
+        quote: "What are you saying?",
+        level: 3,
+        health: 125,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Felix2.gif",
+        image: "resources/characters/hirelings/Felix_lBlade_Front.gif",
+        slimeCost: 40,
+        slots: {}
+      }
+    },
+    levelFour: {
+      karst: {
+        name: "Karst",
+        quote: "I would never do anything as terrible as breaking my word.",
+        level: 4,
+        health: 150,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Karst.gif",
+        image: "resources/characters/hirelings/Karst_Front.gif",
+        slimeCost: 50,
+        slots: {}
+      },
+      agatio: {
+        name: "Agatio",
+        quote: "Our town hangs on the brink of extinction",
+        level: 4,
+        health: 160,
+        unique: true,
+        icon: "resources/characters/hirelings/hireicons/Agatio.gif",
+        image: "resources/characters/hirelings/Agatio_Front.gif",
+        slimeCost: 60,
+        slots: {}
+      }
+    }
+  }
+}
+
+function drawHirelings(){
+
+}
+
+function drawSlots(){
+
+}
+
+function drawItems(){
+
+}
+
+function drawResources(){
+
+}
+
+function djinnActivate(){
+
+}
+
+function drawSelectionMenu(){
+
+}
+
+
 
 
